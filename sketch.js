033f9a0d27e644f9fa3079385e8b4d9eb495b1ff
Dylpickle2008 <dylanrigby1016@gmail.com>
2025-03-07 13:32:39 +0000

    changed game

--- sketch.js
+++ sketch.js
@@ -1,56 +1,107 @@
-//custom variables for y-coordinate of sun & horizon
-let horizon = 500;
-let sunHeight = 600;                         //initial sunheight
-//variables for color change
-let redVal = 0;
-let greenVal = 0;
-function setup() {
-  createCanvas(1600, 900);
-}
+let circleX;
+let circleY;
+let circleRadius;
+let circleMaximumRadius;
+let circleColor;
+let score = 0;
+let highScore;
 
+function setup() {
+  createCanvas(720, 400);
+  colorMode(HSB);
+  noStroke();
+  ellipseMode(RADIUS);
+  textSize(36);
+
+  // Get the last saved high score
+  highScore = getItem('high score');
+
+  // If no score was saved, start with a value of 0
+  if (highScore === null) {
+    highScore = 0;
+  }
+}
 
 function draw() {
-    background(redVal, greenVal, 0);
-    //sun
-    fill(255, 135, 5, 60);
-    circle(300, sunHeight, 180);
-    fill(255, 100, 0, 100);
-    circle(300, sunHeight, 140);
-  
-  
-    // with if-else statement
-  if(sunHeight < horizon) {
-    background("lightblue");               // blue sky if above horizon
+  background(20);
+
+  // If the circle had not shrunk completely
+  if (circleRadius > 0) {
+    // Draw the circle
+    fill(circleColor);
+    circle(circleX, circleY, circleRadius);
+    describeElement('Circle', 'Randomly colored shrinking circle');
+
+    // Shrink it
+    circleRadius -= 1;
+
+    // Show the score
+    textAlign(RIGHT, TOP);
+    fill(220);
+    text(score, width - 20, 20);
+    describeElement('Score', `Text with current score: ${score}`);
   } else {
-    background(0);                         // night sky otherwise
-  }
-
-  //sun
-  fill("yellow");
-  circle(750, sunHeight, 300);
-
-  // draw line for horizon
-  stroke("green");
-  line(0,horizon,1600,horizon);
-
-  //grass
-  fill("green"); 
-  rect(0, horizon, 1600, 500);
- 
-  //sun follows y-coordinate of mouse
-  sunHeight = mouseY;
-
-  //light blue background if sun is above horizon
-  if(sunHeight > horizon){
-  background("black");
+    // Otherwise show the start/end screen
+    endGame();
   }
 }
 
+function startGame() {
+  // Reset the score to 0
+  score = 0;
 
+  // Start with the circle's radius maximum at half the minimum canvas dimension
+  circleMaximumRadius = min(height / 5, width / 5);
+  resetCircle();
+}
 
+function endGame() {
+  // Pause the sketch
+  noLoop();
 
+  // Store the new high score
+  highScore = max(highScore, score);
+  storeItem('high score', highScore);
 
+  textAlign(CENTER, CENTER);
+  fill(220);
+  let startText = `Circle Clicker
+  Click the circle before it gets too small
+  Score: ${score}
+  High score: ${highScore}
+  Click to play`;
+  text(startText, 0, 0, width, height);
+  describeElement('Start text', `Text reading, "${startText}"`);
+}
 
+function resetCircle() {
+  // Start with the circle's radius at its maximum value
+  circleRadius = circleMaximumRadius;
+  circleX = random(circleRadius, width - circleRadius);
+  circleY = random(circleRadius, height - circleRadius);
+  circleColor = color(random(240, 360), random(40, 80), random(50, 90));
+}
 
+function mousePressed() {
+  // If the game is unpaused
+  if (isLooping() === true) {
+    // Check how far the mouse is from the circle
+    let distanceToCircle = dist(mouseX, mouseY, circleX, circleY);
 
+    // If the mouse is closer to the circle's center than the circle's radius,
+    // that means the player clicked on it
+    if (distanceToCircle < circleRadius) {
+      // Decrease the maximum radius, but don't go below 15
+      circleMaximumRadius = max(circleMaximumRadius - 1, 15);
+      resetCircle();
 
+      // Give the player a point
+      score += 1;
+    }
+    // If the game is paused
+  } else {
+    // Start and unpause it
+    startGame();
+    loop();
+  }
+}
